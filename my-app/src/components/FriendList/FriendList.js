1bf01b90fb7ae6b77fccec12371f862cf085c14b
NataliyaKulibaba <natakulibaba88@gmail.com>
2022-02-03 12:16:51 +0200

    correct css

--- my-app/src/components/FriendList/FriendList.js
+++ my-app/src/components/FriendList/FriendList.js
@@ -4,7 +4,8 @@ import s from'./FriendList.module.css'
 export default function FriendList({ isOnline, src, name }) {
   
   return <li className={s.item} >
-    <span className={s.status}>{isOnline}</span>
+    {isOnline?<span className={s.status}>{isOnline}</span>:<span className={s.statusOffline}>{isOnline}</span>}
+    
     <img className={s.avatar} src={src} alt="User avatar" width="48" />
     <p className={s.name}>{ name}</p>
 </li>
